f7d252f6b4c7485c76df7250941fe9275bdfc264
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2014-09-29 22:42:24 +0900

    update

--- app/controller.go
+++ app/controller.go
@@ -14,47 +14,47 @@ func NewController() *Controller {
 	c := &Controller{ksatriya.NewController()}
 
 	c.AddBeforeFilter(c.Before)
-	c.AddAfterFilter(c.After)
-
 	c.GET("/", c.Index)
 	c.GET("/user/:name", c.User)
 	c.GET("/text", c.Text)
 	c.GET("/json", c.JSON)
 	c.GET("/redirect", c.Redirect)
+	c.AddAfterFilter(c.After)
 
 	return c
 }
 
-func (c *Controller) Before(ctx ksatriya.Ctx) {
-	ctx.View().SetTmplDirPath("app/view")
-	ctx.View().SetBaseTmplPath("layout.html")
+func (c *Controller) Before(ctx *ksatriya.Context) {
+	if ctx.Request.URL.Path == "/redirect" {
+		ctx.Redirect("/")
+	}
 }
 
-func (c *Controller) After(ctx ksatriya.Ctx) {
-	ctx.SetRenderArg("title", "ksatriya-sample")
-}
-
-func (c *Controller) Index(ctx ksatriya.Ctx) {
+func (c *Controller) Index(ctx *ksatriya.Context) {
 	ctx.HTML(http.StatusOK, "index.html", nil)
 }
 
-func (c *Controller) User(ctx ksatriya.Ctx) {
+func (c *Controller) User(ctx *ksatriya.Context) {
 	name := ctx.Param("name")
 	ctx.HTML(http.StatusOK, "user.html", ksatriya.RenderArgs{
 		"name": name,
 	})
 }
 
-func (c *Controller) Text(ctx ksatriya.Ctx) {
+func (c *Controller) Text(ctx *ksatriya.Context) {
 	ctx.Text(http.StatusOK, "sample text")
 }
 
-func (c *Controller) JSON(ctx ksatriya.Ctx) {
+func (c *Controller) JSON(ctx *ksatriya.Context) {
 	ctx.JSON(http.StatusOK, map[string]string{
 		"key": "value",
 	})
 }
 
-func (c *Controller) Redirect(ctx ksatriya.Ctx) {
-	ctx.Redirect("/")
+func (c *Controller) Redirect(ctx *ksatriya.Context) {}
+
+func (c *Controller) After(ctx *ksatriya.Context) {
+	ctx.SetTmplDirPath("app/view")
+	ctx.SetBaseTmplPath("layout.html")
+	ctx.SetRenderArg("title", "ksatriya-sample")
 }
