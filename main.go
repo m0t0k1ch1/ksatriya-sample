ea95f5bd59356d9f15666d9f83c14db2d5a96d50
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-05-09 11:37:41 +0900

    use go-server-starter

--- main.go
+++ main.go
@@ -4,22 +4,27 @@ import (
 	"net"
 	"net/http"
 
-	"github.com/lestrrat/go-server-starter-listener"
+	"github.com/lestrrat/go-server-starter/listener"
 	"github.com/m0t0k1ch1/ksatriya-sample/app"
 )
 
 func main() {
 	app := ksatriyasample.NewApp()
 
-	listener, _ := ss.NewListener()
-	if listener == nil {
-		var err error
-		listener, err = net.Listen("tcp", ":8080")
+	listeners, err := listener.ListenAll()
+	if err != nil {
+		panic(err)
+	}
+	var l net.Listener
+	if len(listeners) == 0 {
+		l, err = net.Listen("tcp", ":8080")
 		if err != nil {
 			panic(err)
 		}
+	} else {
+		l = listeners[0]
 	}
 
 	server := &http.Server{Handler: app}
-	server.Serve(listener)
+	server.Serve(l)
 }
