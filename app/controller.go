eb1ab72f1c18360fe11190871974d1a9efc36b83
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2015-10-25 01:28:49 +0900

    use ksatriya.Ctx

--- app/controller.go
+++ app/controller.go
@@ -10,36 +10,36 @@ type Controller struct {
 	*ksatriya.Controller
 }
 
-func (c *Controller) Before(ctx *ksatriya.Context) {
+func (c *Controller) Before(ctx ksatriya.Ctx) {
 	if ctx.Req().URL.Path == "/redirect" {
 		ctx.Redirect("/")
 	}
 }
 
-func (c *Controller) Index(ctx *ksatriya.Context) {
+func (c *Controller) Index(ctx ksatriya.Ctx) {
 	ctx.HTML(http.StatusOK, "index.html", nil)
 }
 
-func (c *Controller) User(ctx *ksatriya.Context) {
+func (c *Controller) User(ctx ksatriya.Ctx) {
 	name := ctx.Arg("name")
 	ctx.HTML(http.StatusOK, "user.html", ksatriya.RenderArgs{
 		"name": name,
 	})
 }
 
-func (c *Controller) Text(ctx *ksatriya.Context) {
+func (c *Controller) Text(ctx ksatriya.Ctx) {
 	ctx.Text(http.StatusOK, "sample text")
 }
 
-func (c *Controller) JSON(ctx *ksatriya.Context) {
+func (c *Controller) JSON(ctx ksatriya.Ctx) {
 	ctx.JSON(http.StatusOK, map[string]string{
 		"key": "value",
 	})
 }
 
-func (c *Controller) Redirect(ctx *ksatriya.Context) {}
+func (c *Controller) Redirect(ctx ksatriya.Ctx) {}
 
-func (c *Controller) After(ctx *ksatriya.Context) {
+func (c *Controller) After(ctx ksatriya.Ctx) {
 	v := ctx.View()
 	v.SetRenderArg("title", "ksatriya-sample")
 
